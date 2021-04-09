b2e5e5cb775c4aa657e5b0d9481f28900a4b4f11
Thái Dương Nguyễn <nguyenthaiduong283@gmail.com>
2021-04-09 18:02:09 +0700

    09/04

---
+++ LiteCommerce.DataLayers/IEmployeeDAL.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LiteCommerce.DomainModels;
+namespace LiteCommerce.DataLayers
+{
+    /// <summary>
+    /// Định nghĩa các phép xử lý dữ liệu liên quan dến nhân viên 
+    /// </summary>
+    public interface IEmployeeDAL
+    {
+        /// <summary>
+        /// Lấy danh sách toàn bộ nhân viên
+        /// </summary>
+        /// <returns></returns>
+        List<Employee> List();
+    }
+}
